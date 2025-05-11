6acd5339854700758b5b8060254fb5ecb4d3fa03
oSnoopyy <mattia.olivamedin@studenti.unipd.it>
2025-05-11 09:59:19 +0200

    Test 6
    close #5

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,9 +6,13 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
+        }
+
         // Definizione delle lettere romane e dei loro valori corrispondenti
-        String[] romanLetters = {"I"};
-        int[] values = {1};
+        String[] romanLetters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         StringBuilder romanNumber = new StringBuilder();
 
